45d4b4fe965516e72226b13e5372650246785dc0
quocviet064 <thqviet12c5.ncthanh@gmail.com>
2024-10-05 10:33:56 +0700

    create-homepage-ui

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,19 @@
+import ArtFengShui from "@/components/ui/home/ArtFengShui"
+import FeaturedKoi from "@/components/ui/home/FeaturedKoi"
+import HomeHero from "@/components/ui/home/HomeHero"
+import Introduciton from "@/components/ui/home/Introduciton"
 
-
-
-const Home = () => {
-
-    return(
-        <div className="pt-32">Home</div>
-    )
+function Home() {
+  return (
+    <div className="space-y-28">
+      <HomeHero />
+      <div className="space-y-40 px-20">
+        <ArtFengShui />
+        <FeaturedKoi />
+      </div>
+      <Introduciton />
+    </div>
+  )
 }
 
-export default Home;
+export default Home
