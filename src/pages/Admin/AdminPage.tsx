fa46a60264079fc14c1538a48de7e83415a18807
MaiSteve <stevemai2002@gmail.com>
2024-11-11 13:17:06 +0700

    clean

--- src/pages/Admin/AdminPage.tsx
+++ src/pages/Admin/AdminPage.tsx
@@ -1,12 +1,10 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 
 import {
   IconBadgeAdFilled,
-  IconBrandTabler,
   IconCreditCard,
   IconFish,
   IconPackage,
-  IconSettings,
   IconUsersGroup,
   IconWaterpolo,
   IconWriting,
@@ -14,93 +12,17 @@ import {
   IconZodiacAquarius
 } from "@tabler/icons-react"
 import { motion } from "framer-motion"
-import { TbMoodEmpty } from "react-icons/tb"
 import { Link } from "react-router-dom"
 
 import { cn } from "@/lib/utils"
 
-import Breadcrumb from "@/components/ui/Breadcrums"
-
-import {
-  Sidebar,
-  SidebarBody,
-  SidebarLink
-} from "../Admin/components/CustomSidebar"
+import { FloatingDock } from "@/components/ui/FloatingDock"
+import { Spotlight } from "@/components/ui/Spotlight"
 
 export function AdminPage() {
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
-
-  const links = [
-    {
-      label: "Thống kê",
-      href: "#",
-      icon: (
-        <IconBrandTabler className="h-5 w-5 flex-shrink-0 text-neutral-700" />
-      )
-    },
-    {
-      label: "Quản lý blog",
-      href: "/admin/blogs",
-      icon: <IconWriting className="h-5 w-5 flex-shrink-0 text-neutral-700" />
-    },
-    {
-      label: "Quản lý gói quảng cáo",
-      href: "/admin/goi-quang-cao",
-      icon: <IconPackage className="h-5 w-5 flex-shrink-0 text-neutral-700" />
-    },
-    {
-      label: "Quản lý người dùng",
-      href: "/admin/nguoi-dung",
-      icon: (
-        <IconUsersGroup className="h-5 w-5 flex-shrink-0 text-neutral-700" />
-      )
-    },
-    {
-      label: "Quản lý các gói quảng cáo",
-      href: "/admin/quang-cao",
-      icon: (
-        <IconBadgeAdFilled className="h-5 w-5 flex-shrink-0 text-neutral-700" />
-      )
-    },
-    {
-      label: "Quản lý các giao dịch",
-      href: "/admin/giao-dich",
-      icon: (
-        <IconCreditCard className="h-5 w-5 flex-shrink-0 text-neutral-700" />
-      )
-    },
-    {
-      label: "Quản lí mệnh",
-      href: "/admin/quan-li-menh",
-      icon: (
-        <IconZodiacAquarius className="h-5 w-5 flex-shrink-0 text-neutral-700" />
-      ) // Icon liên quan đến mệnh hoặc cung hoàng đạo
-    },
-    {
-      label: "Quản lí cá và hồ",
-      href: "/admin/quan-li-ca-va-ho",
-      icon: <IconFish className="h-5 w-5 flex-shrink-0 text-neutral-700" /> // Icon cá và hồ
-    },
-    {
-      label: "Quản lí loại cá và loại hồ",
-      href: "/admin/quan-li-loai-ca-va-loai-ho",
-      icon: <IconWaterpolo className="h-5 w-5 flex-shrink-0 text-neutral-700" /> // Icon cá hoặc hồ
-    },
-    {
-      label: "Quản lí Mệnh tương thích",
-      href: "/admin/quan-li-menh-tuong-thich",
-      icon: <IconYinYang className="h-5 w-5 flex-shrink-0 text-neutral-700" /> // Icon cá hoặc hồ
-    },
-    {
-      label: "Cài đặt",
-      href: "/",
-      icon: <IconSettings className="h-5 w-5 flex-shrink-0 text-neutral-700" />
-    }
-  ]
-
-  const [open, setOpen] = useState(false)
   return (
     <div
       className={cn(
@@ -108,18 +30,6 @@ export function AdminPage() {
         "h-screen"
       )}
     >
-      <Sidebar open={open} setOpen={setOpen}>
-        <SidebarBody className="justify-between gap-10">
-          <div className="flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-            <div className="mt-8 flex flex-col gap-2">
-              {links.map((link, idx) => (
-                <SidebarLink key={idx} link={link} />
-              ))}
-            </div>
-          </div>
-          <div></div>
-        </SidebarBody>
-      </Sidebar>
       <Dashboard />
     </div>
   )
@@ -151,13 +61,69 @@ export const LogoIcon = () => {
 }
 
 const Dashboard = () => {
+  const links = [
+    {
+      title: "Quản lý blog",
+      icon: <IconWriting className="h-full w-full text-neutral-300" />,
+      href: "/admin/blogs"
+    },
+
+    {
+      title: "Quản lí gói quảng cáo",
+      icon: <IconPackage className="h-full w-full text-neutral-300" />,
+      href: "/admin/goi-quang-cao"
+    },
+    {
+      title: "Quản lý người dùng",
+      icon: <IconUsersGroup className="h-full w-full text-neutral-300" />,
+      href: "/admin/nguoi-dung"
+    },
+    {
+      title: "Quản lý quảng cáo của người dùng",
+      icon: <IconBadgeAdFilled className="h-full w-full text-neutral-300" />,
+      href: "/admin/quang-cao"
+    },
+
+    {
+      title: "Quản lý giao dịch",
+      icon: <IconCreditCard className="h-full w-full text-neutral-300" />,
+      href: "/admin/giao-dich"
+    },
+    {
+      title: "Quản lí mệnh",
+      href: "/admin/quan-li-menh",
+      icon: <IconZodiacAquarius className="h-full w-full text-neutral-300" />
+    },
+    {
+      title: "Quản lí cá và hồ",
+      href: "/admin/quan-li-ca-va-ho",
+      icon: <IconFish className="h-full w-full text-neutral-300" />
+    },
+    {
+      title: "Quản lí loại cá và loại hồ",
+      href: "/admin/quan-li-loai-ca-va-loai-ho",
+      icon: <IconWaterpolo className="h-full w-full text-neutral-300" />
+    },
+    {
+      title: "Quản lí Mệnh tương thích",
+      href: "/admin/quan-li-menh-tuong-thich",
+      icon: <IconYinYang className="h-full w-full text-neutral-300" />
+    },
+  ]
   return (
     <div className="flex flex-1">
-      <div className="flex h-full w-full flex-1 flex-col gap-2 rounded-tl-2xl border border-neutral-200 bg-white p-10">
-        <Breadcrumb />
-        <div className="flex flex-grow flex-col items-center justify-start text-black">
-          <TbMoodEmpty size={140} />
-          <p className="mt-5 font-semibold">Dashboard cho admin</p>
+      <div className="bg-grid-white/[0.02] relative flex h-screen w-full overflow-hidden rounded-md bg-black/[0.96] antialiased md:items-center md:justify-center">
+        <Spotlight
+          className="-top-40 left-0 md:-top-20 md:left-60"
+          fill="white"
+        />
+        <div className="relative z-10 mx-auto w-full max-w-7xl p-4 pt-20 md:pt-0">
+          <h1 className="bg-opacity-50 bg-gradient-to-b from-neutral-50 to-neutral-400 bg-clip-text py-3 text-center text-4xl font-bold text-transparent md:text-7xl">
+            Quản lý <br /> hệ thống
+          </h1>
+          <div className="flex h-[35rem] w-full items-center justify-center">
+            <FloatingDock mobileClassName="translate-y-20" items={links} />
+          </div>
         </div>
       </div>
     </div>
